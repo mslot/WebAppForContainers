df6bd71f5d92d2b15061e61c33348d3ff65ec85a
Martin Slot <mas@impact.dk>
2019-12-09 21:43:59 +0100

    Renamed cmdlet calls

--- WebAppForContainers/KeyVault/CreateKeyVaultIfNotExists.ps1
+++ WebAppForContainers/KeyVault/CreateKeyVaultIfNotExists.ps1
@@ -10,14 +10,14 @@ param(
     [string]$Sku = "Standard"
  )
     
-$keyVaultResourceInfo=Get-AzureRmResource -ResourceName $Name -ResourceGroupName $ResourceGroupName
+$keyVaultResourceInfo=Get-AzResource -ResourceName $Name -ResourceGroupName $ResourceGroupName
   
 if($keyVaultResourceInfo) {
     Write-Host "Already set up vault"
     Write-Host $keyVaultResourceInfo
 }
 else {
-    New-AzureRmKeyVault `
+    New-AzKeyVault `
     -Name $Name `
     -Location $Location `
     -EnabledForDeployment `
